bf18f7061b8bf2a3166f43a94a79154505446da1
suganthigowtham <148368475+suganthigowtham@users.noreply.github.com>
2024-05-15 19:12:32 +0530

    Add files via upload

---
+++ Project 2/TC_PIM_01/Data/data.py
@@ -0,0 +1,11 @@
+class WebData:
+
+    def __init__(self):
+        self.url = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
+        self.username = "Admin"
+        self.resetCodeURL = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/sendPasswordReset"
+
+        #test_header data
+        self.url = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
+        self.username = "Admin"
+        self.password = "admin123"
---
+++ Project 2/TC_PIM_01/Locator/locator.py
@@ -0,0 +1,17 @@
+
+class WebLocator:
+
+    def __init__(self):
+        self.usernameLocator = '//*[@id="app"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[1]/div/div[2]/input'
+        self.fypLocator = '//*[@id="app"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[4]/p'
+        self.UserNameLocator = '//*[@id="app"]/div[1]/div[1]/div/form/div[1]/div/div[2]/input'
+        self.resetPasswordLocator = '//*[@id="app"]/div[1]/div[1]/div/form/div[2]/button[2]'
+
+
+#test_header locator
+
+        self.usernameLocator = '//*[@id="app"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[1]/div/div[2]/input'
+        self.passwordLocator =  '//*[@id="app"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[2]/div/div[2]/input'
+        self.loginLocator = '//*[@id="app"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button'
+        self.adminLocator = '//*[@id="app"]/div[1]/div[1]/aside/nav/div[2]/ul/li[1]/a'
+        self.optionsLocator = '//*[@id="app"]/div[1]/div[1]/aside/nav/div[2]/ul/li'
